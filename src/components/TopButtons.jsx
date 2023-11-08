d826407a48e68f088a6ac7f688d61bb9b9adfea8
Róbert Plávka <robert.plavka@student.tuke.sk>
2023-11-08 15:40:08 +0100

    First commit

---
+++ src/components/TopButtons.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+function TopButtons() {
+
+  const cities = [
+    {
+      id: 1,
+      title: 'london'
+    },
+    {
+      id: 2,
+      title: 'tokyo'
+    },
+    {
+      id: 3,
+      title: 'moscow'
+    },
+    {
+      id: 4,
+      title: 'bratislava'
+    },
+    {
+      id: 5,
+      title: 'prague'
+    },
+  ]
+
+  return (
+    <div className='flex items-center justify-around my-6'>
+      {cities.map((city) => (
+        <button className='text-white text-lg font-medium capitalize'>
+          {city.title}
+        </button>
+      ))}
+    </div>
+  )
+}
+
+export default TopButtons
